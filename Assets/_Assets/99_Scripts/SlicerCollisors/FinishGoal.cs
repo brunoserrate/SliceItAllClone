12daee114d1ef2a28050837027f301a225500dd5
brunoserrate <brunostj@hotmail.com>
2023-08-03 10:32:51 -0300

    feat: added win condition script

---
+++ Assets/_Assets/99_Scripts/SlicerCollisors/FinishGoal.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SerrateDevs.SliceItAllClone {
+    public class FinishGoal : MonoBehaviour, ISlicerCollisor
+    {
+        public static event Action<float> OnPlayerWin;
+
+        [SerializeField] private float _scoreMultiplier = 1f;
+        public void OnSlicerHandleHit(PlayerController playerController) {
+            playerController.JumpBack();
+        }
+
+        public void OnSlicerSharpEdgeHit(PlayerController playerController) {
+            PlayerWin(playerController);
+        }
+
+        private void PlayerWin(PlayerController playerController) {
+            playerController.Stuck();
+
+            Debug.Log("Player Win");
+
+            OnPlayerWin?.Invoke(_scoreMultiplier);
+        }
+    }
+}
